332a693b2127260d377b35ba713fae6297252dde
Alice De Lorenci <alice@pdu-b08-g.enst.fr>
2024-02-25 18:07:15 +0100

    DQN with target network: variation 3: larger net

--- src/train.py
+++ src/train.py
@@ -37,17 +37,14 @@ class ProjectAgent:
         self.device = self.test_device
 
         print("Loading model from", self.path)
-        self.model = self.network()
-        self.model.load_state_dict(torch.load(self.path, map_location=self.device))
+        self.model = torch.jit.load(self.path, map_location=self.device)
         self.model.eval()
     
     def __init__(self):
         print("Initializing agent")
 
-        #### INSTANCE ATTRIBUTES ####
-        self.algorithm = "DQNtarget"               
-        self.new = False                    # whether to train and save new model or just load an existing model (models/model.<extension>)
-        self.path = "./src/models/model.pt" # path to default model
+
+        self.path = "./src/models/model_jit.pt" # path to default model
 
         self.n_actions = env.action_space.n             # number of actions
         self.state_dim = env.observation_space.shape[0] # state space dimension
@@ -56,64 +53,16 @@ class ProjectAgent:
         self.device = None
         self.train_device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # device to use for training
         self.test_device = torch.device("cpu")                                           # device to use for evaluation
-        #############################
 
         
-        # command line argument specifies:
-        # - whether to train a new model: python3 main.py new
-        # - or to load an existing model: python3 main.py <file_name>
-        # otherwise, the default model is loaded
+        # command line argument specifies model to load (otherwise, the default model is loaded):
+        # python3 main.py <file_name>
         if( len(sys.argv) > 1 ):        
-            if sys.argv[1] == "new":
-                self.new = True
-            else:
-                self.path = './src/models/'+sys.argv[1]
+            self.path = './src/models/'+sys.argv[1]
 
-        # if the model to load does not exist, train a new model
-        if not self.new and not os.path.exists( self.path ):
-            print("No model found. Training a new model.")
-            self.new = True
-        
-        # train new model
-        if self.new:
-            timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")       # time stamp to avoid overwriting models
-            self.path = "./src/models/{}_{}.pt".format(self.algorithm, timestamp)
-            ep_length, disc_rewards, tot_rewards, V0 = self.train()
-            self.save(self.path)
-
-            self.config['network'] = self.network
-            self.save_setup(self.config, self.path)
-
-            plt.figure()
-            plt.plot(ep_length, label="training episode length")
-            if self.config['monitoring_nb_trials']>0:
-                plt.plot(tot_rewards, label="MC eval of total reward")
-            plt.legend()
-            plt.savefig(self.path+'-fig1.png')
-
-            if self.config['monitoring_nb_trials']>0:
-                plt.figure()
-                plt.plot(disc_rewards, label="MC eval of discounted reward")
-                plt.plot(V0, label="average $max_a Q(s_0)$")
-                plt.legend()
-                plt.savefig(self.path+'-fig2.png')
-
-    
-    def network(self, nb_neurons=[512, 512, 512]):
-        """
-        Neural Network architecture for DQN.
-        """
-        layers = []
-        layers.append(nn.Linear(self.state_dim, nb_neurons[0]))
-        layers.append(nn.ReLU())
-        for i in range(len(nb_neurons)-1):
-            layers.append(nn.Linear(nb_neurons[i], nb_neurons[i+1]))
-            layers.append(nn.ReLU())
-        layers.append(nn.Linear(nb_neurons[-1], self.n_actions))
-        DQN = nn.Sequential(*layers).to(self.device)
-        
-        return DQN
-
+        # if the model to load does not exist, raise an error
+        if not os.path.exists( self.path ):
+            raise Exception("Model {} does not exist.".format(self.path))
     
     def greedy_action(self, network, state):
         """
